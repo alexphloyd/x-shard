3ec18dd0bcdd5047c7be19d8db3c8484ca7eb625
alexphloyd <alexphloyd@gmail.com>
2024-03-10 00:33:42 +0100

    add create_deep_immutable_proxy util

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-export function parse_proxy_target(value: unknown) {
+export function parse_proxy_target<T>(value: T): T {
 	if (!is_object(value)) {
 		throw new Error(
 			`Initial store value should be an object. 
@@ -8,7 +8,28 @@ export function parse_proxy_target(value: unknown) {
 	return value;
 }
 
-function is_object(value: unknown) {
+export function create_deep_immutable_proxy<T>(target: T): T {
+	if (!is_object(target)) {
+		return target;
+	} else {
+		return new Proxy(target as any, {
+			get(target, prop) {
+				return create_deep_immutable_proxy(target[prop as keyof typeof target]);
+			},
+			set() {
+				throw new Error('Snapshot is immutable.');
+			},
+			deleteProperty() {
+				throw new Error('Snapshot is immutable.');
+			},
+			setPrototypeOf() {
+				throw new Error('Snapshot is immutable.');
+			},
+		});
+	}
+}
+
+function is_object<T>(value: T) {
 	return (
 		value instanceof Object &&
 		!(value instanceof Map) &&
