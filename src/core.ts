f5dcf6d0c908410f72a5462c8c38fde54c8005f9
alexphloyd <alexphloyd@gmail.com>
2024-03-09 19:04:33 +0100

    fix: allow all values to be set in store

--- src/core.ts
+++ src/core.ts
@@ -10,6 +10,10 @@ export function createEvent<T extends EventPayload | void = void>() {
 	return emitter;
 }
 
+type EventEmitter<P extends EventPayload | void = void> = ReturnType<
+	typeof createEvent<P>
+>;
+
 export function createStore<T extends ProxyTarget>(initial?: T) {
 	const store_changed_event_key = crypto.randomUUID();
 	const proxy_target = initial
@@ -21,23 +25,33 @@ export function createStore<T extends ProxyTarget>(initial?: T) {
 			return target[prop as keyof T];
 		},
 		set(target, prop, passed_value) {
-			if (passed_value) {
-				target[prop as keyof T] = passed_value;
-				document.dispatchEvent(new Event(store_changed_event_key));
-			}
+			target[prop as keyof T] = passed_value;
+			document.dispatchEvent(new Event(store_changed_event_key));
 			return Boolean(passed_value);
 		},
 	});
 
 	return {
 		/**
-		 * @description $.get() allow to get a snapshot.
+		 * @description $.get() allow to get a store instance
 		 * */
 		get: (prop?: keyof T) => (prop ? $[prop] : $),
 		/**
 		 * @description $.on(event, handler) - allow to handle emitted events.
+		 * Handler has an access to the store and event details.
 		 * */
-		on: (event_emitter: any, handler: any) => {
+		on: <
+			E extends EventEmitter<any>,
+			P extends Parameters<EventEmitter<Parameters<E>[0]>>[0]
+		>(
+			event_emitter: E,
+			handler: (
+				store: typeof $,
+				event: {
+					payload: P;
+				}
+			) => void
+		) => {
 			document.addEventListener(
 				event_keys_storage.get(event_emitter),
 				(kernel_event) => handler($, { payload: kernel_event.detail })
@@ -45,6 +59,7 @@ export function createStore<T extends ProxyTarget>(initial?: T) {
 		},
 		/**
 		 * @description $.watch() allows running an effect after the store has changed.
+		 * Handler has an access to the store.
 		 * */
 		watch: (handler: (store: typeof $) => void) => {
 			document.addEventListener(store_changed_event_key, () =>
@@ -63,7 +78,7 @@ export function parse_proxy_target(value: unknown) {
 	if (!is_valid)
 		throw new Error(
 			`Initial store value should be an object. 
-			 You can store serializable values as $store property.`
+			 You can store non-serializable values as $store property.`
 		);
 	return value;
 }
