5d791e45721a446930e7ffcc61dfea27a7625217
alexphloyd <alexphloyd@gmail.com>
2024-03-10 00:34:08 +0100

    add test for immutable snapshot

--- __tests__/store.test.ts
+++ __tests__/store.test.ts
@@ -3,12 +3,45 @@ import { createEvent, createStore } from '../src/core';
 
 describe('store', () => {
 	const helper_event = createEvent();
-	class User {
+	class Helper {
 		constructor() {}
 		say() {
-			return 'hello';
+			return 'test';
 		}
 	}
+
+	test('snapshot cannot be mutated', () => {
+		const $ = createStore({
+			string: 'text',
+			number: 18,
+			boolean: true,
+			n: null,
+			u: undefined,
+			big_int: BigInt(12),
+			object: {},
+			deep_object: {
+				string: 'text',
+				instance: new Helper(),
+			},
+		});
+		const snapshot = $.get();
+
+		expect(() => (snapshot.string = 'new text')).toThrowError();
+		expect(() => (snapshot.string = {} as any)).toThrowError();
+		expect(() => (snapshot.number = 20)).toThrowError();
+		expect(() => (snapshot.number = 'text' as any)).toThrowError();
+		expect(() => (snapshot.boolean = false)).toThrowError();
+		expect(() => (snapshot.boolean = {} as any)).toThrowError();
+		expect(() => (snapshot.n = 'not null' as any)).toThrowError();
+		expect(() => (snapshot.n = undefined as any)).toThrowError();
+		expect(() => (snapshot.u = 'not undefined' as any)).toThrowError();
+		expect(() => (snapshot.u = undefined)).toThrowError();
+		expect(() => (snapshot.big_int = BigInt(10))).toThrowError();
+		expect(() => ((snapshot.object as any).newProperty = 'new value')).toThrowError();
+		expect(() => (snapshot.deep_object.instance = 'new deep text' as any)).toThrowError();
+
+		expect($.get()).toEqual(snapshot);
+	});
 
 	test('allow to create only with object as initial', () => {
 		expect(() => createStore(25 as any)).toThrowError();
@@ -16,14 +49,16 @@ describe('store', () => {
 	});
 
 	test('create with serializable values', () => {
-		const $ = createStore({ ok: { s: 'text' } });
-		expect($.get().ok.s).toStrictEqual('text');
+		const initial = { ok: { google: 'world' } };
+		const $ = createStore(initial);
+
+		expect($.get()).toEqual(initial);
 	});
 
 	test('create with non-serializable values', () => {
-		const $ = createStore({ ok: { user: new User(), map: new Map() } });
+		const $ = createStore({ ok: { user: new Helper(), map: new Map() } });
 
-		expect($.get().ok.user).toBeInstanceOf(User);
+		expect($.get().ok.user).toBeInstanceOf(Helper);
 		expect($.get().ok.map).toBeInstanceOf(Map);
 	});
 
