d2b07a44177b56ede3694e68334b80ce14170b81
alexphloyd <alexphloyd@gmail.com>
2024-03-12 14:56:09 +0100

    implement base scheduler

--- src/core.ts
+++ src/core.ts
@@ -1,6 +1,34 @@
 import { create_deep_immutable_proxy, parse_proxy_target } from './utils';
 
-const event_keys_storage = new WeakMap();
+// K: emitter, V: event_key
+const event_keys_storage = new WeakMap<EventEmitter<any>, string>();
+
+// K: proxy_target, V: event_key
+const store_changed_event_keys_storage = new WeakMap<ProxyTarget, string>();
+
+const create_scheduler = () => {
+	const jobs = new Map<ProxyTarget, Array<Job>>();
+
+	return {
+		init_target(target: ProxyTarget) {
+			jobs.set(target, []);
+		},
+		post_job(target: ProxyTarget, job: Job) {
+			jobs.get(target)?.push(job);
+		},
+		execute_jobs(target: ProxyTarget) {
+			const target_jobs = jobs.get(target);
+			const event_key = store_changed_event_keys_storage.get(target);
+
+			if (target_jobs?.length && event_key) {
+				target_jobs.forEach((job) => job());
+				document.dispatchEvent(new Event(event_key));
+			}
+		},
+	};
+};
+
+export const scheduler = create_scheduler();
 
 export function createEvent<T extends EventPayload | void = void>() {
 	const key = crypto.randomUUID();
@@ -12,17 +40,19 @@ export function createEvent<T extends EventPayload | void = void>() {
 	return emitter;
 }
 
-export function createStore<S extends StoreInterface>(initial: S = {} as S) {
+export function createStore<S extends ProxyTarget>(initial: S = {} as S) {
 	const store_changed_event_key = crypto.randomUUID();
 	const proxy_target = parse_proxy_target(initial);
+
+	scheduler.init_target(proxy_target);
+	store_changed_event_keys_storage.set(proxy_target, store_changed_event_key);
 
 	const $ = new Proxy(proxy_target, {
 		get(target, prop) {
 			return target[prop as keyof S];
 		},
 		set(target, prop, passed_value) {
-			target[prop as keyof S] = passed_value;
-			document.dispatchEvent(new Event(store_changed_event_key));
+			scheduler.post_job(proxy_target, () => (target[prop as keyof S] = passed_value));
 			return true;
 		},
 	});
@@ -42,17 +72,18 @@ export function createStore<S extends StoreInterface>(initial: S = {} as S) {
 			event_emitter: E,
 			handler: (
 				store: typeof $,
-				snapshot: typeof immutable_proxy,
 				event: {
 					payload: ExtractEventPayload<E>;
 				}
 			) => void
 		) => {
 			const event_key = event_keys_storage.get(event_emitter);
-			const _handler = (kernel_event: CustomEvent) =>
-				handler($, immutable_proxy, { payload: kernel_event.detail });
+			const _handler = (kernel_event: CustomEvent) => {
+				handler($, { payload: kernel_event.detail });
+				scheduler.execute_jobs(proxy_target);
+			};
 
-			document.addEventListener(event_key, _handler);
+			document.addEventListener(event_key as any, _handler); // event key type?
 		},
 		/**
 		 * @description $.watch() allows running an effect after the store has changed.
@@ -64,7 +95,7 @@ export function createStore<S extends StoreInterface>(initial: S = {} as S) {
 	};
 }
 
-interface StoreInterface {
+interface ProxyTarget {
 	[key: string]: any;
 }
 
@@ -73,3 +104,5 @@ type EventEmitter<P extends EventPayload | void = void> = ReturnType<typeof crea
 type EventPayload = Record<string, any> | string | number | boolean;
 
 type ExtractEventPayload<Emitter> = Emitter extends EventEmitter<infer P> ? P : never;
+
+type Job = () => void;
