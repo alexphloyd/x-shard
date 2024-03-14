9e59813b9ee229894cbfb901456f914a060e94ab
alexphloyd <alexphloyd@gmail.com>
2024-03-14 13:40:52 +0100

    refactor proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,34 +2,18 @@ import { ProxyTarget, scheduler } from './core';
 import { is_object } from './parse-object';
 
 export function create_deep_writable_proxy<T extends ProxyTarget>(proxy_target: T, deep?: unknown): T {
-	if (deep) {
-		return new Proxy(deep, {
-			get(target, prop) {
-				return is_object(target[prop as keyof typeof target])
-					? create_deep_writable_proxy(proxy_target, target[prop as keyof typeof target])
-					: target[prop as keyof typeof target];
-			},
-			set(target: any, prop: any, passed_value: unknown) {
-				const value = is_object(passed_value)
-					? create_deep_writable_proxy(proxy_target, passed_value)
-					: passed_value;
-				scheduler.post_job(proxy_target, () => (target[prop] = value));
-				return true;
-			},
-		}) as any;
-	}
-
-	return new Proxy(proxy_target, {
-		get(target: any, prop: any) {
-			return is_object(target[prop])
-				? create_deep_writable_proxy(proxy_target, target[prop])
-				: target[prop];
+	const current_target = deep != null ? deep : proxy_target;
+	return new Proxy(current_target, {
+		get(target, prop) {
+			return is_object(target[prop as keyof typeof target])
+				? create_deep_writable_proxy(proxy_target, target[prop as keyof typeof target])
+				: target[prop as keyof typeof target];
 		},
-		set(target, prop, passed_value) {
+		set(target: any, prop: any, passed_value: unknown) {
 			const value = is_object(passed_value)
 				? create_deep_writable_proxy(proxy_target, passed_value)
 				: passed_value;
-			scheduler.post_job(proxy_target, () => (target[prop as keyof T] = value));
+			scheduler.post_job(proxy_target, () => (target[prop] = value));
 			return true;
 		},
 	}) as T;
