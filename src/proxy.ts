c64226a1332f8bc884632c5ba51f50a4d5f136df
alexphloyd <alexphloyd@gmail.com>
2024-03-14 13:45:13 +0100

    refactor: proxy types

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,9 +4,9 @@ import { is_object } from './parse-object';
 export function create_deep_writable_proxy<T extends ProxyTarget>(proxy_target: T, deep?: unknown): T {
 	const current_target = deep != null ? deep : proxy_target;
 	return new Proxy(current_target, {
-		get(target, prop) {
-			return is_object(target[prop as keyof typeof target])
-				? create_deep_writable_proxy(proxy_target, target[prop as keyof typeof target])
+		get(target: any, prop: any) {
+			return is_object(target[prop])
+				? create_deep_writable_proxy(proxy_target, target[prop])
 				: target[prop as keyof typeof target];
 		},
 		set(target: any, prop: any, passed_value: unknown) {
