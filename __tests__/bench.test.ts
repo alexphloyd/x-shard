7be0a21064ef118418e52e8334ad5efa0f7ef892
alexphloyd <alexphloyd@gmail.com>
2024-03-15 17:59:24 +0100

    process

--- __tests__/bench.test.ts
+++ __tests__/bench.test.ts
@@ -3,7 +3,7 @@ import { configureStore, createSlice } from '@reduxjs/toolkit';
 import { createStore, createEvent } from '../src/core';
 
 describe('bench', () => {
-	const BENCH_TIMES = 35_000;
+	const BENCH_TIMES = 10_000;
 
 	test('s', () => {
 		console.log('---------> update 4 store', BENCH_TIMES + ' times');
@@ -93,82 +93,82 @@ describe('bench', () => {
 		console.timeEnd('redux');
 	});
 
-	test('s-2', () => {
-		console.log('---------> update 1 store', BENCH_TIMES + ' times');
+	// test('s-2', () => {
+	// 	console.log('---------> update 1 store', BENCH_TIMES + ' times');
 
-		console.time('s-2');
+	// 	console.time('s-2');
 
-		const event = createEvent<string>();
-		const $ = createStore({ a: { b: { c: ' 12', d: '2', e: '2' } } });
-		createStore({ a: { b: { c: ' 12', d: '2', e: '2' } } });
-		createStore({ a: { b: { c: ' 12', d: '2', e: '2' } } });
-		createStore({ a: { b: { c: ' 12', d: '2', e: '2' } } });
+	// 	const event = createEvent<string>();
+	// 	const $ = createStore({ a: { b: { c: ' 12', d: '2', e: '2' } } });
+	// 	createStore({ a: { b: { c: ' 12', d: '2', e: '2' } } });
+	// 	createStore({ a: { b: { c: ' 12', d: '2', e: '2' } } });
+	// 	createStore({ a: { b: { c: ' 12', d: '2', e: '2' } } });
 
-		$.on(event, (store, event) => {
-			store.a.b.c = event.payload;
-		});
+	// 	$.on(event, (store, event) => {
+	// 		store.a.b.c = event.payload;
+	// 	});
 
-		for (let i = 0; i < BENCH_TIMES; ++i) {
-			event('test');
-		}
+	// 	for (let i = 0; i < BENCH_TIMES; ++i) {
+	// 		event('test');
+	// 	}
 
-		console.timeEnd('s-2');
-	});
+	// 	console.timeEnd('s-2');
+	// });
 
-	test('redux', () => {
-		console.time('redux-2');
-		const initialState = { a: { b: { c: ' 12', d: '2', e: '2' } } };
-		const s = createSlice({
-			name: 's',
-			initialState,
-			reducers: {
-				updateTest: (state, action) => {
-					state.a.b.c = action.payload;
-				},
-			},
-		});
-		const s2 = createSlice({
-			name: 's2',
-			initialState,
-			reducers: {
-				updateTest: (state, action) => {
-					state.a.b.c = action.payload;
-				},
-			},
-		});
-		const s3 = createSlice({
-			name: 's3',
-			initialState,
-			reducers: {
-				updateTest: (state, action) => {
-					state.a.b.c = action.payload;
-				},
-			},
-		});
-		const s4 = createSlice({
-			name: 's4',
-			initialState,
-			reducers: {
-				updateTest: (state, action) => {
-					state.a.b.c = action.payload;
-				},
-			},
-		});
+	// test('redux', () => {
+	// 	console.time('redux-2');
+	// 	const initialState = { a: { b: { c: ' 12', d: '2', e: '2' } } };
+	// 	const s = createSlice({
+	// 		name: 's',
+	// 		initialState,
+	// 		reducers: {
+	// 			updateTest: (state, action) => {
+	// 				state.a.b.c = action.payload;
+	// 			},
+	// 		},
+	// 	});
+	// 	const s2 = createSlice({
+	// 		name: 's2',
+	// 		initialState,
+	// 		reducers: {
+	// 			updateTest: (state, action) => {
+	// 				state.a.b.c = action.payload;
+	// 			},
+	// 		},
+	// 	});
+	// 	const s3 = createSlice({
+	// 		name: 's3',
+	// 		initialState,
+	// 		reducers: {
+	// 			updateTest: (state, action) => {
+	// 				state.a.b.c = action.payload;
+	// 			},
+	// 		},
+	// 	});
+	// 	const s4 = createSlice({
+	// 		name: 's4',
+	// 		initialState,
+	// 		reducers: {
+	// 			updateTest: (state, action) => {
+	// 				state.a.b.c = action.payload;
+	// 			},
+	// 		},
+	// 	});
 
-		const store = configureStore({
-			reducer: {
-				s: s.reducer,
-				s2: s2.reducer,
-				s3: s3.reducer,
-				s4: s4.reducer,
-			},
-		});
+	// 	const store = configureStore({
+	// 		reducer: {
+	// 			s: s.reducer,
+	// 			s2: s2.reducer,
+	// 			s3: s3.reducer,
+	// 			s4: s4.reducer,
+	// 		},
+	// 	});
 
-		for (let i = 0; i < BENCH_TIMES; ++i) {
-			store.dispatch(s.actions.updateTest('s'));
-		}
-		console.timeEnd('redux-2');
-	});
+	// 	for (let i = 0; i < BENCH_TIMES; ++i) {
+	// 		store.dispatch(s.actions.updateTest('s'));
+	// 	}
+	// 	console.timeEnd('redux-2');
+	// });
 
 	test('s-3', () => {
 		console.log('---------> execute handler without mutations', BENCH_TIMES + ' times');
@@ -243,47 +243,47 @@ describe('bench', () => {
 		console.timeEnd('redux-3');
 	});
 
-	test('s-3', () => {
-		console.log('---------> single store mutation', BENCH_TIMES + ' times ');
+	// test('s-3', () => {
+	// 	console.log('---------> single store mutation', BENCH_TIMES + ' times ');
 
-		console.time('s-4');
+	// 	console.time('s-4');
 
-		const event = createEvent<string>();
-		const $ = createStore({ a: { b: { c: ' 12', d: '2', e: '2' } } });
+	// 	const event = createEvent<string>();
+	// 	const $ = createStore({ a: { b: { c: ' 12', d: '2', e: '2' } } });
 
-		$.on(event, (store, event) => {
-			store.a.b.c = event.payload;
-		});
+	// 	$.on(event, (store, event) => {
+	// 		store.a.b.c = event.payload;
+	// 	});
 
-		for (let i = 0; i < BENCH_TIMES; ++i) {
-			event('test');
-		}
+	// 	for (let i = 0; i < BENCH_TIMES; ++i) {
+	// 		event('test');
+	// 	}
 
-		console.timeEnd('s-4');
-	});
+	// 	console.timeEnd('s-4');
+	// });
 
-	test('redux-3', () => {
-		console.time('redux-3');
-		const initialState = { a: { b: { c: ' 12', d: '2', e: '2' } } };
-		const s = createSlice({
-			name: 's',
-			initialState,
-			reducers: {
-				updateTest: (state, action) => {
-					state.a.b.c = action.payload;
-				},
-			},
-		});
+	// test('redux-3', () => {
+	// 	console.time('redux-3');
+	// 	const initialState = { a: { b: { c: ' 12', d: '2', e: '2' } } };
+	// 	const s = createSlice({
+	// 		name: 's',
+	// 		initialState,
+	// 		reducers: {
+	// 			updateTest: (state, action) => {
+	// 				state.a.b.c = action.payload;
+	// 			},
+	// 		},
+	// 	});
 
-		const store = configureStore({
-			reducer: {
-				s: s.reducer,
-			},
-		});
+	// 	const store = configureStore({
+	// 		reducer: {
+	// 			s: s.reducer,
+	// 		},
+	// 	});
 
-		for (let i = 0; i < BENCH_TIMES; ++i) {
-			store.dispatch(s.actions.updateTest('s'));
-		}
-		console.timeEnd('redux-3');
-	});
+	// 	for (let i = 0; i < BENCH_TIMES; ++i) {
+	// 		store.dispatch(s.actions.updateTest('s'));
+	// 	}
+	// 	console.timeEnd('redux-3');
+	// });
 });
