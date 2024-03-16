a0744f76cc0d37c21d535e9cf995914d1846e000
alexphloyd <alexphloyd@gmail.com>
2024-03-16 15:30:31 +0100

    playground process

--- __tests__/schdeuling.test.ts
+++ __tests__/schdeuling.test.ts
@@ -23,25 +23,22 @@ describe('scheduling', () => {
 		expect($.get().session?.id).toEqual(12);
 	});
 
-	test('super-test', () => {
+	test('playground', () => {
 		const $ = createStore<{ session?: Session }>({ session: undefined });
 
 		const session_defined = createEvent<Session>();
 		const try_certificate = createEvent();
 
-		session_defined({ id: 12, isVerified: false });
-		try_certificate();
-
 		$.on(session_defined, (store, event) => {
 			store.session = event.payload; // post_job
-			console.log(store, 'before try');
-			// try_certificate(); // why it execute jobs ? -> any event handler of $ execute existed jobs
-			console.log(store, 'after try');
+
+			try_certificate(); // execute -- ?
 		});
 
-		$.on(try_certificate, (store) => {
-			console.log(store, 'IN TRY');
-		});
+		$.on(try_certificate, (store) => {});
+
+		session_defined({ id: 12, isVerified: false });
+		try_certificate();
 
 		console.log('hello');
 	});
