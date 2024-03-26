7679329c245d1ee1aa4950bf3b5d6b11035b9618
alexphloyd <alexphloyd@gmail.com>
2024-03-26 08:54:27 +0100

    add base react bind test

--- __tests__/react/use-store.test.tsx
+++ __tests__/react/use-store.test.tsx
@@ -1,29 +1,40 @@
-import { describe, test } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
+import { act, render, screen } from '@testing-library/react';
 
 import { createEvent, createStore } from '~/core/src/core';
 import { useStore } from '~/react/src/core';
 
 describe('react bind', () => {
-	const $main = createStore({ counter: 5 });
+	const initial_value = 5;
 	const incremented = createEvent();
-
+	const $main = createStore({ counter: initial_value });
 	$main.on(incremented, (store) => {
 		store.counter += 1;
 	});
-	$main.on('document:DOMContentLoaded', (store) => {
-		store.counter += 10;
-	});
 
+	let render_times = 0;
 	function App() {
+		render_times += 1;
 		const $ = useStore($main);
-
-		return <p>{$.counter}</p>;
+		return <p data-testid='content'>{$.counter}</p>;
+	}
+	function get_app_value() {
+		return Number(screen.getByTestId('content').innerText);
 	}
 
-	test('use-store', () => {
+	beforeEach(() => {
+		render_times = 0;
 		render(<App />);
+	});
 
-		screen.debug();
+	test('initial render', () => {
+		expect(render_times).toEqual(1);
+		expect(get_app_value()).toEqual(initial_value);
+	});
+
+	test('store mutation should trigger render once', () => {
+		act(incremented);
+		expect(render_times).toEqual(2);
+		expect(get_app_value()).toEqual(initial_value + 1);
 	});
 });
