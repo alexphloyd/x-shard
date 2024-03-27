86b22894633fb618c4fe1cc79b8946aae004f28b
alexphloyd <alexphloyd@gmail.com>
2024-03-27 21:21:48 +0100

    fix readonly value by path

--- utils/get-store-value-by-path.ts
+++ utils/get-store-value-by-path.ts
@@ -1,9 +1,9 @@
-import { type Store } from '~/core/src/core';
+import { type AnyStore } from '~/core/src/core';
 
-export function get_store_value_by_path<S extends Store<any>, P extends ResourcePath<ReturnType<S['get']>>>(
+export function get_store_value_by_path<S extends AnyStore, P extends ResourcePath<ReturnType<S['get']>>>(
 	store: S,
 	path: P
-): DefineTypeByPath<ReturnType<S['get']>, P> {
+): Readonly<DefineTypeByPath<ReturnType<S['get']>, P>> {
 	const arr = path.split('.');
 	let target = store.get();
 
@@ -11,5 +11,5 @@ export function get_store_value_by_path<S extends Store<any>, P extends Resource
 		target = target[arr[i]];
 	}
 
-	return target as DefineTypeByPath<ReturnType<S['get']>, P>;
+	return target as Readonly<DefineTypeByPath<ReturnType<S['get']>, P>>;
 }
