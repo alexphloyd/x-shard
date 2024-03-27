0e57067f6bc8f9baa1239923f22ba1dd98b0be21
alexphloyd <alexphloyd@gmail.com>
2024-03-27 21:22:41 +0100

    process

--- packages/react/src/core.ts
+++ packages/react/src/core.ts
@@ -1,13 +1,26 @@
-import { type Store } from '~/core/src/core';
+import { type AnyStore } from '~/core/src/core';
 import { useEffect, useReducer } from 'react';
-import { get_store_value_by_path } from 'utils/get_store_value_by_path';
+import { get_store_value_by_path } from 'utils/get-store-value-by-path';
 
-export function useStore<S extends Store>(store: S, path?: ResourcePath<ReturnType<S['get']>>) {
+export function useStore<S extends AnyStore, P extends ResourcePath<ReturnType<S['get']>>>(
+	store: S,
+	path: P
+): Readonly<DefineTypeByPath<ReturnType<S['get']>, P & string>>;
+export function useStore<S extends AnyStore>(store: S): ReturnType<S['get']>;
+export function useStore<S extends AnyStore, P extends ResourcePath<ReturnType<S['get']>> | void = void>(
+	store: S,
+	path?: P
+): ReturnType<S['get']> | Readonly<DefineTypeByPath<ReturnType<S['get']>, P & string>> {
 	const [, force] = useReducer((x) => !x, true);
+
 	useEffect(() => {
 		const untrack = store.track(() => force());
 		return untrack;
 	}, [store]);
 
-	return path ? get_store_value_by_path(store, path) : store.get();
+	if (typeof path === 'string' && path?.length) {
+		return get_store_value_by_path(store, path);
+	} else {
+		return store.get();
+	}
 }
